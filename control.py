e927694169c11adfe4321d2fa797faae2acbce68
Zac <zachng1@gmail.com>
2020-03-05 09:52:14 +1300

    Updated help text

--- control.py
+++ control.py
@@ -64,12 +64,27 @@ def getargs(arglist):
 
 def usage():
     print(
-            """Usage: 
-            -c num implements the 'cutup' method, whereby Burroughs would randomly cut pages and shuffle them about. num here signifies the max length possible for a cut. 
-            -f num implements the 'foldin' method, whereby Burroughs would fold two pages in half and layer them down the middle. num here signifies how many words to read from one text before 'folding in' the next.
-            In both cases, specify at least 3 .docx files: 2 to mix together, and the 3rd as output. The output file need not already exist.
-            You can mix as many files as you like, however the output will be as long as the shortest"""
-            )
+            """
+            Usage: 
+            control.py [-mh][-f num] [-c num] file1.docx file2.docx ... fileN.docx output.docx
+
+            Where either -f or -c must be used. 
+
+            -c num: implements the 'cutup' method. num signifies the max length possible for a cut. i.e. if num=10, you will end up with continuous lines of text between 1 and 10 from each text.  
+
+            -f num: implements the 'foldin' method. num signifies how many words to read from one text before 'folding in' the next.
+
+            -h: prints help.
+
+            -m: optional flag, specifies that you are mixing audio files.
+
+            In all cases, specify at least 3 files: 2 to mix together, and the 3rd as output. The output file need not already exist. You can mix as many files as you like, however the wordcount of the output will be as long as that of the shortest.
+
+            When mixing text, please use .docx or .doc files. 
+
+            When mixing audio, you *should* be able to use any format supported by pydub/ffmpeg and the script will pick up the format from the file extension. You can mix any number of formats into any output format -- when specifying output, make sure to add the extension you want.
+            """
+                        )
 
     
 
